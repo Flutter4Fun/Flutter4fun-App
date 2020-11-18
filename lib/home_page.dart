606cd093b34b367982f303d5c65446c7747815b7
imaNNeoFighT <iman.neofight@gmail.com>
2020-11-19 00:47:17 +0330

    Added UI-Challenge 2 and 3

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,15 +1,46 @@
 import 'package:flutter/material.dart';
 
 import 'package:ui_challenge_1/ui_challenge_1.dart';
+import 'package:ui_challenge_2/ui_challenge_2.dart';
+import 'package:ui_challenge_3/ui_challenge_3.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: FlatButton(onPressed: () {
-          Navigator.of(context).push(new MaterialPageRoute(builder: (context) => UiChallenge1()));
-        }, child: Text("UI Challenge 1")),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FlatButton(
+              onPressed: () {
+                Navigator.of(context).push(new MaterialPageRoute(
+                    builder: (context) => UiChallenge1()));
+              },
+              child: Text(
+                "UI Challenge 1",
+              ),
+            ),
+            FlatButton(
+              onPressed: () {
+                Navigator.of(context).push(new MaterialPageRoute(
+                    builder: (context) => UiChallenge2()));
+              },
+              child: Text(
+                "UI Challenge 2",
+              ),
+            ),
+            FlatButton(
+              onPressed: () {
+                Navigator.of(context).push(new MaterialPageRoute(
+                    builder: (context) => UiChallenge3()));
+              },
+              child: Text(
+                "UI Challenge 3",
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
